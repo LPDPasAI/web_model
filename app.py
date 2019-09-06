3402c3d7336a6d2753f666aa69463d0ce5d2f903
leandro <depersiis.l@gmail.com>
2019-09-06 17:52:57 +0200

    Prova 2

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ from flask import Flask, render_template, request
 
 app = Flask('web_model')
 
-
+@app.route('/')
+def show_predict_stock_form():
+    return render_template('predictorform.html')
 
 
